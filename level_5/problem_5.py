d3cc853ce0fb46c803eb45f1de3b82aa009e2959
Nando <kaeslin.nando@gmail.com>
2023-09-15 13:02:43 +0200

    wip

--- level_5/problem_5.py
+++ level_5/problem_5.py
@@ -2,21 +2,26 @@
 def solution(grid):
 
     def count(y, z):
-        # counts number of active occupied fields
-        # from vertical position y
-        if y == 0:
-            b = z & 3
-            return bin(b).count("1")  # b.bit_count()
-        b = 0
-        b |= z & (1 << y)
-        b |= z & (1 << (y + 1))
-        b |= z & (1 << (n + 1))
-        return bin(b).count("1")  # b.bit_count()
+        cnt = 0
+        if z & (1 << y):
+            cnt += 1
+        if z & (2 << y):
+            cnt += 1
+        if z & (1 << extra_bit_position):
+            cnt += 1
+        return cnt
+        # b = z & (3 << y)
+        # return bin(b).count("1")  # b.bit_count()
 
     def post_check(x, y, z, is_extra):
         # returns if the current occupations are permitted
         tot = count(y, z) + int(is_extra)
         return not (grid[y][x] ^ (tot == 1))
+    
+    # def post_check_2(x, y, z, is_extra):
+    #     # returns if the current occupations are permitted
+    #     tot = count(y, z) + int(is_extra)
+    #     return not (grid[y][x] ^ (tot == 1))
     
     def pre_check(x, y, z):
         if not grid[y][x]: return True
@@ -42,20 +47,28 @@ def solution(grid):
         else:
             next_y = y + 1
             next_x = x
+        # if y == m - 1:
+        #     next_extra_bit = z & 1 != 0
+        # else:
         next_extra_bit = z & (1 << (y + 1)) != 0
         if y == 0:
             next_zs = [set_bit(z, extra_bit_position, next_extra_bit) for _ in range(4)]
-            next_zs = [set_bit(nz, y, nb) for nz, nb in zip(next_zs, [0, 0, 1, 1])]
-            next_zs = [set_bit(nz, y + 1, nb) for nz, nb in zip(next_zs, [0, 1, 0, 1])]
+            next_zs = [set_bit(nz, y, nb) for nz, nb in zip(next_zs, [0, 1, 0, 1])]
+            next_zs = [set_bit(nz, y + 1, nb) for nz, nb in zip(next_zs, [0, 0, 1, 1])]
         else:
             next_zs = [set_bit(z, extra_bit_position, next_extra_bit) for _ in range(2)]
             next_zs = [set_bit(nz, y + 1, nb) for nz, nb in zip(next_zs, [0, 1])]
+        if x == 0:
+            next_zs = [set_bit(nz, extra_bit_position, 0) for nz in next_zs]
+            next_zs += [set_bit(nz, extra_bit_position, 1) for nz in next_zs]
         return [(next_x, next_y, nz) for nz in next_zs]
 
 
     def dp(x, y, z):
+        if x == n:
+            return 1
         # print x, y, z
-        print(cache[x][y][z])
+        # print(cache[x][y][z])
         if cache[x][y][z] != -1:
             return cache[x][y][z]
         # # terminal condition
@@ -64,37 +77,51 @@ def solution(grid):
         if not pre_check(x, y, z):
             cache[x][y][z] = 0
             return 0
-        res = 1
+        res = 0
         extra_bit_tmp = get_extra_bit(z)
+        # current = z & (1 << y)
         next_states = get_next_states(x, y, z)
         for nx, ny, nz in next_states:
             if not post_check(x, y, nz, extra_bit_tmp):
+            # if not post_check_2(x, y, nz, current):
                 continue
-            res *= dp(nx, ny, nz)
+            res += dp(nx, ny, nz)
         cache[x][y][z] = res
         return res 
     
+    
 
     m, n = len(grid), len(grid[0])
-    cache = [[[-1 for _ in range((1 << (m + 3)) - 1)] for _ in range(m)] for _ in range(n + 1)]
+    extra_bit_position = m + 1
 
-    cache[n][0] = [1 for _ in range((1 << (m + 3)) - 1)]
+    cache = [[[-1 for _ in range((1 << (m + 2)) )] for _ in range(m + 1)] for _ in range(n)]
+
+    # cache[n][0] = [1 for _ in range((1 << (m + 2)))]
 
     # print(len(cache), len(cache[0]), len(cache[0][0]))
 
-    extra_bit_position = m + 2
+    
 
-    res = dp(0, 0, 0) + dp(0, 0, 1)
-    # print(cache)
+    res = dp(0, 0, 0) + dp(0, 0, 1 << extra_bit_position)
+    # res = dp(0, 0, 1 << extra_bit_position)
+    print(cache)
 
     return res
 
 
 if __name__ == "__main__":
-    grid = [[True, False, True], [False, True, False], [True, False, True]]
-    grid = [[True]]
-    # grid = [[False]]
-
+    # grid = [[True, False, True], [False, True, False], [True, False, True]]  # 4
+    # grid = [[True]]  # 4
+    # grid = [[False]]  # 12
+    # grid = [[True], [True]]
+    # grid = [[True], [False]]
+    # grid = [[True, False], [False, True]]
+    # grid = [[True, False, True, False, False, True, True, True], [True, False, True, False, False, False, True, False], [True, True, True, False, False, False, True, False], [True, False, True, False, False, False, True, False], [True, False, True, False, False, True, True, True]]
+    # 254
+    # grid = [[True, True, False, True, False, True, False, True, True, False], [True, True, False, False, False, False, True, True, True, False], [True, True, False, False, False, False, False, False, False, True], [False, True, False, False, False, False, True, True, False, False]]
+    # 11567
+    grid = [[True, True, True]]  # 8
+    # grid = [[True], [True], [True]]  # 
 
     res = solution(grid)
     # print "- " * 20
